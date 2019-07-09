817511054145e5de75c16973340e3c75248488dc
Asif malik <31756470+CodeWithNeo@users.noreply.github.com>
2019-07-09 14:20:27 +0530

    Create index.php

---
+++ images/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Refresh" content="7; url=../index.php" />
+  </head>
+  <body>
+    <p>Please login <a href="../index.php">here</a>.</p>
+  </body>
+</html>
